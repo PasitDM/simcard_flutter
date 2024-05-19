99fdd705eb12c5bc3e6cb33cf38058afda4bc438
PasitDM <pasitmosac@gmail.com>
2024-05-19 19:14:21 +0700

    main home refactor ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sim_card_flutter/mobile_number_screen.dart';
 import 'package:sim_card_flutter/sim_card_info_screen.dart';
 import 'package:sim_card_flutter/sim_number_screen.dart';
 
@@ -39,57 +40,96 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SimNumberScreen(),
-                    ),
-                  );
-                },
-                child: const Text('Go to Lib simnumber screen'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SimDataPlusScreen(),
-                    ),
-                  );
-                },
-                child: const Text('Go to Lib sim_data_plus screen'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SimDataPlusScreen(),
-                    ),
-                  );
-                },
-                child: const Text('Go to Lib mobile_number screen'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SimCardInfoScreen(),
-                    ),
-                  );
-                },
-                child: const Text('Go to Lib sim_card_info screen'),
-              ),
-            ],
-          ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            buildContent(
+              context,
+              topic: 'simnumber 0.0.12',
+              dateTxt: 'Jul 29, 2022',
+              btnTxt: 'simnumber',
+              canIos: true,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SimNumberScreen(),
+                  ),
+                );
+              },
+            ),
+            buildContent(
+              context,
+              topic: 'sim_data_plus 0.1.4',
+              dateTxt: 'Aug 26, 2022',
+              btnTxt: 'sim_data_plus',
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SimDataPlusScreen(),
+                  ),
+                );
+              },
+            ),
+            buildContent(
+              context,
+              topic: 'mobile_number 2.1.1',
+              dateTxt: 'Oct 11, 2022',
+              btnTxt: 'mobile_number',
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MobileNumberScreen(),
+                  ),
+                );
+              },
+            ),
+            buildContent(
+              context,
+              topic: 'sim_card_info 1.0.2',
+              dateTxt: 'Apr 24, 2024',
+              btnTxt: 'sim_card_info',
+              canIos: true,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SimCardInfoScreen(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Column buildContent(
+    BuildContext context, {
+    required String topic,
+    required String dateTxt,
+    required String btnTxt,
+    required Function() onTap,
+    bool canAndroid = true,
+    bool canIos = false,
+  }) {
+    return Column(
+      children: [
+        Text(
+          topic,
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        Text('Last updated: $dateTxt'),
+        ElevatedButton(
+          onPressed: onTap,
+          child: Text('Go to Lib $btnTxt screen'),
+        ),
+        Text('Platform: ${canAndroid ? 'Android' : ''}${canIos ? ',iOS' : ''}'),
+        const Divider(height: 20),
+      ],
     );
   }
 }
